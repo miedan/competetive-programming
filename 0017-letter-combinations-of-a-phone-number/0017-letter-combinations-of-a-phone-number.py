a90e7110850aaf6173abee08f2d24263c32172e2
miedan <99678973+miedan@users.noreply.github.com>
2023-03-17 10:46:37 +0300

    Time: 27 ms (90.85%), Space: 13.9 MB (67.88%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,35 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-       
-        phone= {1 :'', 2:'abc', 3:'def', 4:'ghi', 5 :'jkl', 6:'mno', 7:'pqrs', 8:'tuv', 9 : 'wxyz', 0 : ''}
+        phone = {1 : '', 2 : 'abc', 3 : 'def', 4 : 'ghi', 5 : 'jkl', 6 : 'mno', 7 : 'pqrs', 8 : 'tuv', 9 : 'wxyz', 0 : ''}
         
         res = []
-        def backtrack(i,combination):
-            
+        def backtrack (i, combination):
             if len(combination) == len(digits):
                 res.append(''.join(combination))
-                return
-            vaule = phone[int(digits[i])]
-            for j in range(len(vaule)):
-                combination.append(vaule[j])
-                backtrack(i+1,combination)
+                return 
+            d = phone[int(digits[i])]
+            for j in range (len(d)):
+                combination.append(d[j])
+                backtrack( i+1, combination)
                 combination.pop()
+                
         if len(digits )== 0:
             return []
-        if len(digits ) == 1:
-            return phone[int(digits[0])]
-        backtrack(0,[])
         
-        
-        
+        backtrack(0, [])
         return res
-            
-            
-            
         
         
-            
-            
+        
         
         
         
