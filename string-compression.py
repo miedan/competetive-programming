4588ac266d9e308590a70aac7247c51f46cdadc6
miedan <99678973+miedan@users.noreply.github.com>
2023-04-19 14:27:15 +0300

    added string-compression.python

--- string-compression.py
+++ string-compression.py
@@ -1,25 +1,26 @@
-class Solution(object):
-    def compress(self, chars):
-        """
-        :type chars: List[str]
-        :rtype: int
-        """
-        i, j = 0, 0
-        count = 1
-        length = 0
-        while i<len(chars):
-            if i<len(chars)-1 and chars[i] == chars[i+1]:
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        n = len(chars)
+        if n == 1:
+            return 1
+        
+        i = 0
+        j = 0
+        
+        while i < n:
+            count = 1
+            while i < n - 1 and chars[i] == chars[i+1]:
                 count += 1
-            else:
-                chars[j] = chars[i]
-                j += 1
-                length += 1
-                if count>1:
-                    for chr in str(count):
-                        chars[j] = chr
-                        j += 1
-                        length += 1
-                count = 1     
-            i += 1      
+                i += 1
             
-        return length
+            chars[j] = chars[i]
+            j += 1
+            
+            if count > 1:
+                for c in str(count):
+                    chars[j] = c
+                    j += 1
+            
+            i += 1
+        
+        return j
