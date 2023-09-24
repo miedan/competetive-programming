96dd0392dd11749b90178418837ad59ee9fbb2d8
miedan <99678973+miedan@users.noreply.github.com>
2023-09-24 21:10:51 +0300

    added longest-palindromic-subsequence.python

---
+++ longest-palindromic-subsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        @cache
+        def dp(i, j):
+
+            if i > j :
+                return 0
+            
+            if i == j:
+                return 1
+
+            if s[i] == s[j]:
+                return 2 + dp(i+1, j-1)
+            else:
+                return max(dp(i+1, j), dp(i, j-1))
+
+        return dp(0, len(s)-1)
